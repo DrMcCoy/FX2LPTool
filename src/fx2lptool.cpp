f868f941f970adf3a9c91e14ba81eba68b6a9c4d
Sven Hesse <drmccoy@drmccoy.de>
2013-11-10 14:34:35 +0100

    MAIN: Add empty main()

---
+++ src/fx2lptool.cpp
@@ -0,0 +1,27 @@
+/* FX2LPTool - Tool to manipulate Cypress EZ-USB FX2LP devices.
+ *
+ * FX2LPTool is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/** @file fx2lptool.cpp
+ *  The project's main entry point.
+ */
+
+int main(void) {
+	return 0;
+}
